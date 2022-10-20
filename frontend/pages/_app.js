458aa0967fe8bbb4b10c0739ac0fc9585e06a599
MurtazaNeher <murtazaradio@gmail.com>
2022-10-20 22:23:57 +0530

    Fixes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,11 +2,7 @@ import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
-  return 
-  <>
-    <Navbar/>
-    <Component {...pageProps} />
-  </>
+  return <> <Navbar/> <Component {...pageProps} /></>
 }
 
 export default MyApp
